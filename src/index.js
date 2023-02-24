9e2e0d26386dd4d8337b832a258048eed4297f24
Carlos Roberto <carlosrobertocostajunior@id.uff.br>
2023-02-24 18:38:11 -0300

    requisito 01 implementado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  const talkers = fs.readFileSync('src/talker.json');
+  const parsedTalkers = JSON.parse(talkers);
+  if (!parsedTalkers) return [];
+  response.status(HTTP_OK_STATUS).json(parsedTalkers);
 });
 
 app.listen(PORT, () => {
